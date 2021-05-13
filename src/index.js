9b9d1dfd7250ce5c29e9321a28b96e23787963ae
Ржанов Володимир <emitter@10a>
2021-05-13 18:23:59 +0300

    Добавляет задание

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import dishCardsTpl from "./templates/dish-cards.hbs";
+import menu from "./menu.json";
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    menu: document.querySelector('.js-menu'),
+    checkbox: document.querySelector('#theme-switch-toggle'),
+};
+
+
+const createDishCards = menu => refs.menu.insertAdjacentHTML('beforeend', dishCardsTpl(menu));
+
+const makedMenu = createDishCards(menu);
+
+const themeData = {};
+const onChangeTheme = () => {
+    if (refs.checkbox.checked) {
+        themeData.Theme = Theme.DARK;
+        themeData.checked = true;
+        document.body.classList.remove(Theme.LIGHT);
+        document.body.classList.add(Theme.DARK);
+    }
+    else {
+        themeData.Theme = Theme.LIGHT;
+        themeData.checked = false;
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    };
+
+    localStorage.setItem('theme', JSON.stringify(themeData));  
+};
+
+const savedTheme = () => {
+    const currentTheme = localStorage.getItem('theme');
+    const currentThemeParse = JSON.parse(currentTheme);
+
+    if (currentTheme) {
+        document.body.classList.add(currentThemeParse.Theme);
+        refs.checkbox.checked = currentThemeParse.checked;
+    };
+};
+
+savedTheme();
+
+refs.checkbox.addEventListener('change', onChangeTheme);
+
